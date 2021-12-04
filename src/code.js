54a2f787f2a5ddfe7b37b9f5736b67c90076a993
UP2024320 <86617698+UP2024320@users.noreply.github.com>
2021-12-04 07:04:57 +0000

    finish part 2

--- src/code.js
+++ src/code.js
@@ -1,5 +1,6 @@
 let fs = require('fs');
 
+//Part 1
 try {
     let data = fs.readFileSync('./src/paperSize.txt', 'utf8');
     let splitData = data.split('\n');
@@ -12,6 +13,24 @@ try {
              + 2*(Number(dimensions[1]*dimensions[2])) + 2*(Number(dimensions[2]*dimensions[0]));
         count = count + (paperSize+smallest);
     }; console.log(count);
+} catch (error) {
+    console.log(`Error: ${error.stack}`);
+};
+
+//Part2
+try {
+    let data = fs.readFileSync('./src/paperSize.txt', 'utf8');
+    let splitData = data.split('\n');
+    let count = 0;
+    let bowRibbon = 0;
+    let presentRibbon = 0;
+    for(let i = 0; i < splitData.length; i++) {
+        let dimensions = splitData[i].split('x');
+        bowRibbon = Number(dimensions[0])*Number(dimensions[1])*Number(dimensions[2])
+        dimensions.sort(function(a,b){return a-b})
+        presentRibbon = 2*(Number(dimensions[0])+Number(dimensions[1]));
+        count = count + (Number(bowRibbon)+Number(presentRibbon));
+    }; console.log(count, bowRibbon, presentRibbon);
 } catch (error) {
     console.log(`Error: ${error.stack}`);
 };
